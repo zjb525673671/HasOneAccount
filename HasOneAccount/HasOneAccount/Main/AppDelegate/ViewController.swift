88accf38ef3b1ae3ad0e70829219eb1ba9f37259
周结兵 <525673671@qq.com>
2018-09-07 14:11:46 +0800

    项目初始

---
+++ HasOneAccount/HasOneAccount/Main/AppDelegate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HasOneAccount
+//
+//  Created by zhoujiebing on 2018/9/7.
+//  Copyright © 2018年 周结兵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

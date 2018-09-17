6ca3d89979410fd8815f5b0ad833b13a2b5d1231
周结兵 <525673671@qq.com>
2018-09-17 16:30:27 +0800

    主页面

---
+++ HasOneAccount/HasOneAccount/Function/Home/Controller/HomeMainVC.swift
@@ -0,0 +1,35 @@
+//
+//  HomeMainVC.swift
+//  HasOneAccount
+//
+//  Created by zhoujiebing on 2018/9/17.
+//  Copyright © 2018年 周结兵. All rights reserved.
+//
+
+import UIKit
+
+class HomeMainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
